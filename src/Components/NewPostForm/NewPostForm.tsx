ed39c56838c121e00c054ff8a224e94e8ae96a5b
Anneke Miers <annekemiers.am@gmail.com>
2021-04-11 11:38:09 -0600

    Add component base

--- src/Components/NewPostForm/NewPostForm.tsx
+++ src/Components/NewPostForm/NewPostForm.tsx
@@ -1,12 +1,54 @@
+import React, { Component } from 'react';
+import './NewPostForm.scss';
+// import React from 'react'
+class NewPostForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      header = '',
+      body = ''
+    }
+  }
 
-import React from 'react'
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
 
-const NewPostForm = () => {
-  return (
-    <form>
-      {/* should have state and be a class*/}
-    </form>
-  )
+  submitPost = event => {
+    event.preventDefault();
+    const newPost = {
+      id: Date.now(),
+      ...this.state
+    }
+    this.props.addNewPost(newPost)
+    // not sure if we want this new post adding method here
+    this.clearInputs();
+  }
+
+  clearInputs = () => {
+    this.setState({ header: '', body: '' });
+  }
+
+  render() {
+    return (
+      <form>
+        <input
+          type='text'
+          placeholder='Title of your post*'
+          name='title'
+          value={this.state.header}
+          onChange={event => this.handleChange(event)}
+        />
+        <input
+          type='text'
+          placeholder='Body of your post*'
+          name='body'
+          value={this.state.body}
+          onChange={event => this.handleChange(event)}
+        />
+      </form>
+    )
+  }
 }
 
-export default NewPostForm
+export default NewPostForm;
