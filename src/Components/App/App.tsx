7f458337b10b4edc8a6129bc44a3a65493f1a0e7
Paige Vannelli <paigevannelli@gmail.com>
2021-04-11 09:39:44 -0700

    Set up state in app

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -2,7 +2,24 @@
 import './App.scss';
 import React, { Component } from 'react'
 
-class App extends Component {
+interface IAppState {
+  allPosts: Array<object>;
+  error: string;
+}
+
+class App extends Component<IAppState> {
+  constructor(props: IAppState | Readonly<IAppState>) {
+    super(props);
+    this.state = {
+      allPosts: [],
+      error: ''
+    }
+  }
+
+  componentDidMount = () => {
+    //fetch data locally at first 
+  }
+
   render() {
     return (
       <main>
