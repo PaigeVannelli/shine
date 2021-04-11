e43b50bb0b26e7535658a988eec7dd0df7a25377
Paige Vannelli <paigevannelli@gmail.com>
2021-04-11 10:26:44 -0700

    set up imports for MainPage component

--- src/Components/MainPage/MainPage.tsx
+++ src/Components/MainPage/MainPage.tsx
@@ -1,12 +1,15 @@
 
 import React from 'react'
+import Searchbar from '../Searchbar/Searchbar'
+import AllPosts from '../AllPosts/AllPosts'
+import Nav from '../Nav/Nav'
 
 const MainPage = () => {
   return (
     <section>
-      {/* <Searchbar /> */}
-      {/* <AllPosts /> */}
-      {/* <Nav /> */}
+      <Searchbar />
+      <AllPosts />
+      <Nav />
     </section>
   )
 }
