0b5505295fea31052b3d8355659797efa782a6a3
Paige Vannelli <paigevannelli@gmail.com>
2021-04-12 16:45:42 -0700

    Finish nav bar styles without icons

--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -7,7 +7,7 @@ import './Nav.scss'
 
 const Nav = () => {
   return (
-    <nav className='nav-bar'>
+    <footer className='nav-bar'>
       <button className='nav-button'>
         <img src={filterIcon} className='nav-button-image'/>
       </button>
@@ -23,7 +23,7 @@ const Nav = () => {
       <button className='nav-button'>
         <img src={filterIcon} className='nav-button-image'/>
       </button>
-    </nav>
+    </footer>
   )
 }
 
