1c303e110e3896d9cb27e8df9972f0c896bcaae9
Paige Vannelli <paigevannelli@gmail.com>
2021-04-15 15:25:04 -0700

    Write first Cypress test to check setup

---
+++ cypress/integration/shine-spec.ts
@@ -0,0 +1,11 @@
+describe('Feedback Loop', () => {
+  it('Should confirm that true is equal to true', () => {
+    expect(true).to.equal(true)
+  });
+});
+
+describe('Loading Page View', () => {
+  it('Should confirm that a loading page appears before data is fetched', () => {
+    // cy.intercept('')
+  });
+});
