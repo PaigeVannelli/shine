0585d8ae640aa3a3336b963f6d0610e93efcd696
Paige Vannelli <paigevannelli@gmail.com>
2021-04-16 16:58:08 -0700

    Create interfaces for ExpandedPost and pass info into Post component

--- src/Components/ExpandedPost/ExpandedPost.tsx
+++ src/Components/ExpandedPost/ExpandedPost.tsx
@@ -7,13 +7,34 @@ import { IPost } from '../../types'
 import { getPost } from '../../apiCalls';
 // import AllReplies from '../AllReplies/AllReplies'
 
+
+interface ICurrentPost {
+  pid: number
+  uid: number
+  author: string
+  timestamp: number
+  title: string
+  content: string 
+  comments: Array<IReply | undefined>
+}
+
 interface IExpandedPost {
-  replies: Array<any>
-  currentPost: IPost
+  replies: Array<any>;
+  currentPost: {
+    [key: string]: ICurrentPost,
+  };
+};
+
+interface IReply {
+  cid: number;
+  uid: number;
+  author: string;
+  timestamp: number;
+  comment: string;
 }
 
 interface IExpandedPostProps {
-  match?: any
+  match?: any;
 }
 class ExpandedPost extends Component<IExpandedPostProps, IExpandedPost> {
   constructor(props: IExpandedPostProps) {
@@ -26,12 +47,22 @@ class ExpandedPost extends Component<IExpandedPostProps, IExpandedPost> {
   
   componentDidMount = () => {
     getPost(this.props.match.params.id)
+    .then(post => this.setState(prevState => ({
+      ...prevState,
+      currentPost: post.post
+    })))
   }
 
   render() {
     return (
       <section>
-        <Post />
+        <Post 
+          title={this.state.currentPost.post.title}
+          content={this.state.currentPost.post.content}
+          author={this.state.currentPost.post.author}
+          timestamp={this.state.currentPost.post.timestamp}
+          pid={this.state.currentPost.post.pid}
+        />
         {/* <AllReplies />
         <ReplyForm /> */}
       </section>
