41168beaee30cac8bf8d003a1fd10a8834972ad3
Paige Vannelli <paigevannelli@gmail.com>
2021-04-17 12:11:38 -0700

    Pass down post replies data to All Replies component

--- src/Components/AllReplies/AllReplies.tsx
+++ src/Components/AllReplies/AllReplies.tsx
@@ -1,7 +1,25 @@
 //map over all replies and generate Reply components 
 // funcational component 
+import React from 'react'
+import Reply from '../Reply/Reply'
 
-const AllReplies = () => {
+interface IAllReplies {
+  allReplies: Array<any>
+}
+
+const AllReplies = ({allReplies}: IAllReplies) => {
+  const replyData = allReplies.map(reply => {
+    return (
+      <Reply
+        key={reply.cid}
+        author={reply.author}
+        timestamp={reply.timestamp}
+        body={reply.body}
+        cid={reply.cid}
+        uid={reply.uid}
+      />
+    )
+  })
   return (
     <h1>Replies</h1>
   )
