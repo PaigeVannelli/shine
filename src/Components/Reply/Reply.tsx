eb51b39148f70ab4da520a5cee95b46217e7c5c8
Nikki <piknik@me.com>
2021-04-19 13:16:21 -0600

    Add date and time to replies

--- src/Components/Reply/Reply.tsx
+++ src/Components/Reply/Reply.tsx
@@ -1,9 +1,14 @@
 import { IReply } from '../../types'
 
 const Reply = ({key, author, timestamp, body, cid, uid}: IReply) => {
+  let fDate = new Date(timestamp)
+  let timeString = fDate.toLocaleTimeString('en-US')
+  const formattedDate = new Date(fDate).toLocaleString('en-US', {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'})
+
   return (
     <article>
       <h1>{author}</h1>
+        <h5>{formattedDate}, {timeString}</h5>
       <p>{body}</p>
     </article>
   )
